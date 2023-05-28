46ad01e0721e63274875b8b8b8afcbd76aca3fa8
diwakarreddy02 <kasireddydiwakar02@gmail.com>
2023-05-27 20:58:24 -0400

    SetUp

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"></img>
+    </div>
+  );
+}
+
+export default App;
