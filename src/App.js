c94c72f7f9081eb0a92dc23734d8f10bfefcac88
diwakarreddy02 <kasireddydiwakar02@gmail.com>
2023-05-28 15:48:46 -0400

    completed search function

--- src/App.js
+++ src/App.js
@@ -14,12 +14,13 @@ function App() {
       setIsLoading(false);
     })
     .catch(err => console.error(err));
-  }, [])
+  }, [term])
 
   return (
     <div className="container mx-auto">
-      <ImageSearch />
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading</h1>: <div className="grid grid-cols-3 gap-4">
+      <ImageSearch searchText={(text) => setTerm(text)}/>
+      {!isLoading && images.length === 0 && <h1 className="text-4xl text-center mx-auto mt-32">No Images Found...</h1>}
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
           <ImageCard key={image.id} image={image}/>
         ))}
--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ImageSearch = () => {
+const ImageSearch = ({ searchText }) => {
+  const [text, setText] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
+    searchText(text);
+  }
+
   return (
     <div>
-      <form className="w-full max-w-sm">
-        <div className="flex items-center border-b border-b-2 border-teal=500 py-2">
-            <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image Term..." />
+      <form onSubmit={onSubmit} className="w-full max-w-sm m-auto py-7">
+        <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+            <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image Term..." />
             <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
                 Search
             </button>
@@ -15,4 +21,4 @@ const ImageSearch = () => {
   )
 }
 
-export default ImageSearch
+export default ImageSearch;
